4b39efa174d8b750c388eabd6237ece5e6aaa2df
Vladimir Stratiev <greti.yordanova@gmail.com>
2022-12-19 19:06:19 +0200

    initial commit

---
+++ FirstStepsInCoding/src/FirstStepsInCoding/Lab/HelloSoftUni.java
@@ -0,0 +1,9 @@
+package FirstStepsInCoding.Lab;
+
+import java.nio.file.FileSystemNotFoundException;
+
+public class HelloSoftUni {
+    public static void main(String[] args) {
+        System.out.println("Hello SoftUni");
+    }
+}
